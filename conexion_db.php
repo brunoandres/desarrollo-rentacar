2b5f10a5f1256847592c10cafaf66be2aa02c65b
Bruno Andres <brunoandres2013@@gmail.com>
2019-03-20 14:33:17 -0300

    'Archivo_disponibilidad_y_conexion_db'

--- conexion_db.php
+++ conexion_db.php
@@ -1,22 +1,20 @@
 <?php
-class Conexion{
-
-     static function conectar_MySql_server(){
-  
-      $servidor = 'mysql.hostinger.com.ar';
-      $base_de_datos ='u756079281_alqui';
-      $usuario = 'u756079281_bruno';
-      $clave = 'cenergon';
-  
-      $conexion = new mysqli($servidor,$usuario,$clave,$base_de_datos);
-      mysqli_set_charset($conexion,'utf8');
-      if ($conexion->connect_error) {
-  
-        die('Error en la conexion : '.$conexion->connect_errno.'-'.$conexion->connect_error);
-  
-      }
-      return $conexion;
-    }
+/**
+ *
+ */
+class Conexion extends mysqli
+{
+  private $h='mysql.hostinger.com.ar';
+  private $u='u756079281_bruno';
+  private $p='cenergon';
+  private $db='u756079281_alqui';
+  public function __construct()
+  {
+    parent:: __construct($this->h,$this->u,$this->p,$this->db);
+    $this->set_charset('utf-8');
+    $this->connect_errno ? die('Error en la conexion'.mysqli_connect_errno()): $m= "Conexion exitosa";
+    
   }
-  
+}
+
 ?>
