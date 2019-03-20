b549d54499b54bf5e9c05f583f6c6b61812dd48f
Bruno Andres <brunoandres2013@@gmail.com>
2019-03-20 13:48:09 -0300

    'Probando_cambios'

--- js/fechas.js
+++ js/fechas.js
@@ -17,3 +17,20 @@ $('#fecha_hasta_chile').datepicker({
   dateFormat: "dd/mm/yy",
   autoclose : true
 });
+
+
+$(".dp").datepicker();
+
+var dates = jQuery('.start_date, .end_date').datepicker("option", "onSelect", 
+   function(selectedDate){
+       var $this = $(this),
+           option = $this.hasClass("start_date") ? "minDate" : "maxDate",
+           adjust = $this.hasClass("start_date") ? 1 : -1,
+           base_date = new Date(selectedDate),
+           new_date = new Date();
+
+       new_date.setDate(base_date.getDate() + (1 * adjust));
+       dates.not(this).datepicker("option", option, new_date);
+       
+   }
+);
