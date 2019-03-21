2427bd9843036633ee579a3e35f9d876f1ec2ae0
Bruno Andres <brunoandres2013@@gmail.com>
2019-03-21 11:56:24 -0300

    ''

--- verificar_disponibilidad.php
+++ verificar_disponibilidad.php
@@ -4,13 +4,13 @@ ini_set('display_errors', '1');
 
 require('archivo_busca_disponibilidad.php');
 
-$pFechaDesde = $_POST['fecha_desde'];
+/*$pFechaDesde = $_POST['fecha_desde'];
 $pFechaHasta = $_POST['fecha_hasta'];
 $hDesde = '15:00:00';
 $hHasta = '15:00:00';
-$pCategoria = 0;
+$pCategoria = 0;*/
 
-$hay_disponibilidad = buscarDisponibilidad('2019-10-10','2019-10-20',$hDesde,$hHasta,$pCategoria);
+$hay_disponibilidad = buscarDisponibilidad('2019-10-10','2019-10-20','15:00:00','15:00:00',0);
 
 if($hay_disponibilidad>0){
     echo 1;
