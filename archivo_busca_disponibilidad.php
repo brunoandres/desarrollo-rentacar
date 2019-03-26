ed4779d9b84254d8c432c2da0d4983ff220a33d2
Bruno Andres <brunoandres2013@@gmail.com>
2019-03-26 14:14:40 -0300

    ''

--- archivo_busca_disponibilidad.php
+++ archivo_busca_disponibilidad.php
@@ -22,26 +22,27 @@ define("CANTSONIC", 1);
 
 function buscarDisponibilidad($pFechaDesde,$pFechaHasta,$hDesde,$hHasta,$pCategoria){
 
-	$categoria=$pCategoria;
+	$categoria         = $pCategoria;
 	$fechaDesdeReserva = $pFechaDesde;
 	$fechaHastaReserva = $pFechaHasta;
-	$horaDesdeReserva = $hDesde;
-	$horaHastaReserva = $hHasta;
+	$horaDesdeReserva  = $hDesde;
+	$horaHastaReserva  = $hHasta;
 
 	$db = new Conexion();
-	$sql="SELECT * from reservas WHERE vehiculo=$categoria and estado=1 and fhasta >= '2019-01-01' ";
-	//$sql="SELECT * from reservas WHERE estado = 1 and vehiculo=$categoria and fdesde between '$fechaDesdeReserva' and '$fechaHastaReserva'";
+	//Consulto todas las reservas confirmadas donde la fecha sea mayor al año corriente.
+	$sql   = "SELECT * from reservas WHERE vehiculo=$categoria and estado=1 and fhasta >= '2019-01-01' ";
 	$resul = $db->query($sql);
 
-	//RESERVA COMIENZA EN FALSE
-	$reserva= false;
+	//La disponibilidad comienza en estado False.
+	$reserva = false;
 
+	//Switch para contabilizar las cantidades de autos por categoria solicitada.
 	switch ($categoria) {
     case 0://A
         $contador = CANTGOL+CANTCORSA+CANTCLIO+CANTONIX+CANTKWID;
         break;
     case 1://B
-       $contador = CANTLOGAN+CANTVOYAGE;
+       $contador  = CANTLOGAN+CANTVOYAGE;
         break;
     case 2://C
         $contador = CANTSURAN+CANTCRONOS;
@@ -57,31 +58,25 @@ function buscarDisponibilidad($pFechaDesde,$pFechaHasta,$hDesde,$hHasta,$pCatego
     	break;
 	}//fin switch
 
-	//Recorro las reservas evaluando disponibilidad
-	//FUNCIONANDO 30-06-2017
-	/*echo "Cantidad de autos".$contador.'<br>';
-	echo "Reserva solicitada<br>";
-	echo $fechaDesdeReserva.'<br>';
-	echo $fechaHastaReserva.'<br>';*/
-	$sumaDeChoques =0;
+
+	//Creo una variable para ir contabilizando las veces que las fechas no coinciden y no hay disponibilidad.
+	$sumaDeChoques = 0;
 
 	if ($resul = $db->query($sql)) {
-	//$contador = $contador +1;    //fetch associative array 
 	    while ($row = $resul->fetch_assoc()) {
-	        
-	        
-			$reserva= false;
+	         
+			$reserva           = false;
+			//Variable para verificar si puedo entregar un auto en el mismo dia, con un rango de horas definidio mas abajo.
 			$disponibleEnEldia = false;
-	        
-	        $fechaDesdeConfirmada=$row['fdesde'];
-			$fechaHastaConfirmada=$row['fhasta'];
+			
+			//Traigo informacion de las reservas consultadas.
+			$fechaDesdeConfirmada = $row['fdesde'];
+			$fechaHastaConfirmada = $row['fhasta'];
+			$horaDesdeConfirmada  = $row['hdesde'];
+			$horaHastaConfirmada  = $row['hhasta'];
+			$nroReserva           = $row['id_reserva'];
 
-			$horaDesdeConfirmada = $row['hdesde'];
-			$horaHastaConfirmada = $row['hhasta'];
-
-			$nroReserva=$row['id_reserva'];
-
-	       ///Primero evaluo contra las fechas de reservas confirmadas
+			///Primero evaluo contra las fechas de reservas confirmadas
 			//Evaluo que NO este en el rango la fecha
 			if (!check_in_range($fechaDesdeReserva, $fechaHastaReserva, $fechaDesdeConfirmada)) {
 				if (!check_in_range($fechaDesdeReserva, $fechaHastaReserva, $fechaHastaConfirmada))
@@ -92,55 +87,61 @@ function buscarDisponibilidad($pFechaDesde,$pFechaHasta,$hDesde,$hHasta,$pCatego
 				else $reserva = false;				
 			} else $reserva= false;		
 
+
+			//Seteo mi variable margen en False.
 			$margen = false;
 
-			//MARGEN HORARIO
+			//Calulo la diferencia de horas para ver si es posible entregar el auto en el dia.
 			$horaDesdeR = date('H:i:s', strtotime($horaHastaConfirmada .'+ 1 hour'));
 
 			if ($reserva==false and $fechaHastaConfirmada==$fechaDesdeReserva and $horaDesdeR <= $horaDesdeReserva) {			
 				
+				//Voy contabilizando mi contador y activo el margen, es decir hay disponbiilidad.
 				$contador = $contador;
-				$margen = true;
+				$margen   = true;
 				
 			}				
-			if ($reserva==false and $margen==false){	
+			if ($reserva==false and $margen==false){
+				//Descuento vehiculos a mi contador y agrego los choques a mi variable para contabilizar.	
 				$contador = $contador - 1;
-				$sumaDeChoques =$sumaDeChoques+1;
-				
+				$sumaDeChoques =$sumaDeChoques+1;				
 			}
-
 	   }
 	}
 
-	//El contador define si tengo auto disponible
+	/************************************************ 
+
+	IMPORTANTE : EL CONTADOR >= 1 ME DEFINE SI HAY AUTOS DISPONIBLES
+
+	/************************************************* */
 
 	if ($contador > 0){
-		$reserva= true;
-		$msj= 'Si tiene auto';
+
+		$msj	 = 'Si tiene auto';
+		$reserva = true;
 		//echo "SI Tenes auto <br>";
 		//echo $contador;
 	}else{
-		$reserva=false;
-		$msj='falta auto';
+
+		$msj	 = 'falta auto';
+		$reserva = false;		
 	    //echo "NO Tenes auto <br>";
 		//echo $contador;
 	}
 
-	//echo $contador=$msj.$reserva;
-
+	//Retorno una cantidad ( numero ).
 	return $contador;
-	//echo $contador;
-}//fin de la funcion buscarDisponibilidad
-
-
+	
+}
+//Fin funcion
 
 //Funcion que permite evaluar si una fecha esta en un rango determinado
 function check_in_range($start_date, $end_date, $date_from_user)
 {
   // COnvierto a timestamp
   $start_ts = strtotime($start_date);
-  $end_ts = strtotime($end_date);
-  $user_ts = strtotime($date_from_user);
+  $end_ts   = strtotime($end_date);
+  $user_ts  = strtotime($date_from_user);
 
   // Controlo el rango entre start and end
   return (($user_ts >= $start_ts) && ($user_ts <= $end_ts));
--- verificar_disponibilidad.php
+++ verificar_disponibilidad.php
@@ -2,8 +2,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
+//Archivo que contiene la funcion principal de disponibilidad
 require('archivo_busca_disponibilidad.php');
 
+//Recibo los datos principales para comprobar disponibilidad
 $fecha_desde = $_POST['fecha_desde'];
 $fecha_hasta = $_POST['fecha_hasta'];
 $hora_desde = '15:00:00';
@@ -13,8 +15,10 @@ $categoria_auto = $_POST['select_categoria'];
 $hay_disponibilidad = buscarDisponibilidad($fecha_desde,$fecha_hasta,$hora_desde,$hora_hasta,$categoria_auto);
 
 if($hay_disponibilidad>0){
+    //Resultado positivo, existe disponibilidad
     echo 1;
 }else{
+    //No hay disponibilidad
     echo 0;
 }
 
